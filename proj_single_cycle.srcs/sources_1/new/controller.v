c35399b535ebe0585ea28e04a79993c92b1a462f
pacman123 <3028079152@qq.com>
2025-07-08 17:13:40 +0800

    [Refactor]: rewrite controller, easier to debug

--- proj_single_cycle.srcs/sources_1/new/controller.v
+++ proj_single_cycle.srcs/sources_1/new/controller.v
@@ -25,61 +25,121 @@ module Controller (
     // TODOS: debug interface
 );
 
-    assign opcode1 = inst[31:26];
-    assign opcode2 = inst[25];
-    assign opcode3 = inst[24:22];
-    assign opcode4 = inst[21:15];
 
-    always @(*) begin
-        // 3R types
-        if (opcode1 == 6'b0 && opcode2 == 1'b0 && opcode3 == 3'b0) begin
-            npc_op <= NPC_PC_4;
-            rf_sel <= RD_RK;
-            wD_sel <= WD_ALu;
+    wire [5:0] opcode1 = inst[31:26];
+    wire opcode2 = inst[25];
+    wire [2:0] opcode3 = inst[24:22];
+    wire [6:0] opcode4 = inst[21:15];
 
-            alu_sel <= (opcode4 == 7'b0100000 |
-                        opcode4 == 7'b0100010 |
-                        opcode4 == 7'b0101001 |
-                        opcode4 == 7'b0101010 |
-                        opcode4 == 7'b0101011 |
-                        opcode4 == 7'b0100100 |
-                        opcode4 == 7'b0100101
-                        ) ? ASEL_RD2 : ASEL_RD2_5;
+    // 3R
+    wire ADDW   = (opcode1 == 6'b0) && (opcode2 == 1'b0) && (opcode3 == 3'b000) && (opcode4 == 7'b0100000);
+    wire SUBW   = (opcode1 == 6'b0) && (opcode2 == 1'b0) && (opcode3 == 3'b000) && (opcode4 == 7'b0100010);
+    wire AND    = (opcode1 == 6'b0) && (opcode2 == 1'b0) && (opcode3 == 3'b000) && (opcode4 == 7'b0101001);
+    wire OR     = (opcode1 == 6'b0) && (opcode2 == 1'b0) && (opcode3 == 3'b000) && (opcode4 == 7'b0101010);
+    wire XOR    = (opcode1 == 6'b0) && (opcode2 == 1'b0) && (opcode3 == 3'b000) && (opcode4 == 7'b0101011);
+    wire SLLW   = (opcode1 == 6'b0) && (opcode2 == 1'b0) && (opcode3 == 3'b000) && (opcode4 == 7'b0101110);
+    wire SRLW   = (opcode1 == 6'b0) && (opcode2 == 1'b0) && (opcode3 == 3'b000) && (opcode4 == 7'b0101111);
+    wire SRAW   = (opcode1 == 6'b0) && (opcode2 == 1'b0) && (opcode3 == 3'b000) && (opcode4 == 7'b0110000);
+    wire SLT    = (opcode1 == 6'b0) && (opcode2 == 1'b0) && (opcode3 == 3'b000) && (opcode4 == 7'b0100100);
+    wire SLTU   = (opcode1 == 6'b0) && (opcode2 == 1'b0) && (opcode3 == 3'b000) && (opcode4 == 7'b0100101);
 
-            alu_op <=   (opcode4 == 7'b0100000) ? ALU_ADD :
-                        (opcode4 == 7'b0100010) ? ALU_SUB :
-                        (opcode4 == 7'b0101001) ? ALU_AND :
-                        (opcode4 == 7'b0101010) ? ALU_OR :
-                        (opcode4 == 7'b0101011) ? ALU_XOR :
-                        (opcode4 == 7'b0101110) ? ALU_SL :
-                        (opcode4 == 7'b0101111) ? ALU_SRL :
-                        (opcode4 == 7'b0110000) ? ALU_SRA :
-                        (opcode4 == 7'b0100100) ? ALU_LT_S :
-                        (opcode4 == 7'b0100101) ? ALU_LT_U :
-                        4'b0000; /* should not reach here */
-        end
-    end
+    // 2RI5 types
+    wire SLLIW  = (opcode1 == 6'b0) && (opcode2 == 1'b0) && (opcode3 == 3'b001) && (opcode4 == 7'b0000001);
+    wire SRLIW  = (opcode1 == 6'b0) && (opcode2 == 1'b0) && (opcode3 == 3'b001) && (opcode4 == 7'b0001001);
+    wire SRAIW  = (opcode1 == 6'b0) && (opcode2 == 1'b0) && (opcode3 == 3'b001) && (opcode4 == 7'b0010001);
 
-    always @(*) begin
-        // 2RI5 types
-        if (opcode1 == 6'b0 && opcode2 == 1'b0 && opcode3 == 3'b001) begin
-            npc_op <= NPC_PC_4;
-            // no rD2
-            wD_sel <= WD_ALU;
-            alu_sel <= ASEL_INST_5;
-            alu_op <=   (opcode4 == 7'b0000001) ? ALU_SL :
-                        (opcode4 == 7'b0001001) ? ALU_SRL :
-                        (opcode4 == 7'b0010001) ? ALU_SRA :
-                        4'b0; /* should not reach here */
-        end
-    end
+    // 2RI12 types
+    wire ADDIW  = (opcode1 == 6'b0) && (opcode2 == 1'b1) && (opcode3 == 3'b010);
+    wire ANDI   = (opcode1 == 6'b0) && (opcode2 == 1'b1) && (opcode3 == 3'b101);
+    wire ORI    = (opcode1 == 6'b0) && (opcode2 == 1'b1) && (opcode3 == 3'b110);
+    wire XORI   = (opcode1 == 6'b0) && (opcode2 == 1'b1) && (opcode3 == 3'b111);
+    wire SLTI   = (opcode1 == 6'b0) && (opcode2 == 1'b1) && (opcode3 == 3'b000);
+    wire SLTUI  = (opcode1 == 6'b0) && (opcode2 == 1'b1) && (opcode3 == 3'b001);
+    wire LDB    = (opcode1 == 6'b001010) && (opcode2 == 1'b0) && (opcode3 == 3'b000);
+    wire LDBU   = (opcode1 == 6'b001010) && (opcode2 == 1'b1) && (opcode3 == 3'b000);
+    wire LDH    = (opcode1 == 6'b001010) && (opcode2 == 1'b0) && (opcode3 == 3'b001);
+    wire LDHU   = (opcode1 == 6'b001010) && (opcode2 == 1'b1) && (opcode3 == 3'b001);
+    wire LDW    = (opcode1 == 6'b001010) && (opcode2 == 1'b0) && (opcode3 == 3'b010);
+    wire STB    = (opcode1 == 6'b001010) && (opcode2 == 1'b0) && (opcode3 == 3'b100);
+    wire STH    = (opcode1 == 6'b001010) && (opcode2 == 1'b0) && (opcode3 == 3'b101);
+    wire STW    = (opcode1 == 6'b001010) && (opcode2 == 1'b0) && (opcode3 == 3'b110);
 
-    always @(*) begin
-        
-    end
+    // 1RI20
+    wire LU12IW = (opcode1 == 6'b000101) && (opcode2 == 1'b0);
+    wire PCADDU = (opcode1 == 6'b000111) && (opcode2 == 1'b0);
 
+    // 2RI16
+    wire BEQ    = (opcode1 == 6'b010110);
+    wire BNE    = (opcode1 == 6'b010111);
+    wire BLT    = (opcode1 == 6'b011000);
+    wire BLTU   = (opcode1 == 6'b011010);
+    wire BGE    = (opcode1 == 6'b011001);
+    wire BGEU   = (opcode1 == 6'b011011);
+    wire JIRL   = (opcode1 == 6'b010011);
 
+    // I26
+    wire B      = (opcode1 == 6'b010100);
+    wire BL     = (opcode1 == 6'b010101);
 
+    assign alu_op =     (ADDW | ADDIW | LDB | LDBU | LDH | LDHU | LDW | STB | STH | STW) ? ALU_ADD :
+                        (SUBW) ? ALU_SUB :
+                        (OR | ORI) ? ALU_OR :
+                        (XOR | XORI) ? ALU_XOR : 
+                        (SLLW | SLLIW) ? ALU_SL :
+                        (SRLW | SRLIW) ? ALU_SRL :
+                        (SRAW | SRAIW) ? ALU_SRA :
+                        (AND | ANDI) ? ALU_AND :
+                        (BEQ) ? ALU_EQ :
+                        (BNE) ? ALU_NEQ :
+                        (SLT | SLTI | BLT) ? ALU_LT_S :
+                        (SLTU | SLTUI | BLTU) ? ALU_LT_U :
+                        (BGE) ? ALU_GE_S :
+                        (BGEU) ? ALU_GE_U :
+                        4'b0;
 
-    
+    assign alu_sel =    (ADDW | SUBW | AND | OR | XOR | SLT | SLTU) ? ASEL_RD2 :
+                        (SLLW | SRLW | SRAW) ? ASEL_RD2_5 : 
+                        (SLLIW | SRLIW | SRAIW) ? ASEL_INST_5 :
+                        (PCADDU) ? ASEL_INST_20 :
+                        (ADDIW | SLTI | SLTUI | LDB | LDBU | LDH | LDHU | LDW | STB | STH | STW | JIRL) ? ASEL_SEXT1 :
+                        (ANDI | ORI | XORI) ? ASEL_ZEXT :
+                        3'b000;
+                    
+    assign npc_op =     (ADDW | SUBW | AND | OR | XOR | SLLW 
+                        | SRLW | SRAW | SLT | SLTU | SLLIW | SRLIW | SRAIW
+                        | ADDIW | ANDI | ORI | XORI | SLTI | SLTUI
+                        | LDB | LDBU | LDH | LDHU | LDW | STB | STH | STW
+                        | LU12IW | PCADDU
+                        ) ? NPC_PC_4 :
+                        (B | BL) ? NPC_PC_OFF :
+                        (BEQ | BNE | BLT | BLTU | BGE | BGEU) ? NPC_PC_OFF_BR :
+                        (JIRL) ? NPC_PC_OFF :
+                        2'b00;
+
+    assign sext1_op =   (ADDIW | SLTI | SLTUI | LDB | LDBU | LDH | LDHU | LDW | STB | STH | STW) ? SEXT1_12 :
+                        (BEQ | BNE | BLT | BLTU | BGE | BGEU | JIRL) ? SEXT1_16 :
+                        (B | BL) ? SEXT1_28 :
+                        2'b00;
+
+    assign sext2_sel =  (LDB) ? SEXT2_8 :
+                        (LDH) ? SEXT2_16 :
+                        1'b0;
+
+    assign rf_sel =     (ADDW | SUBW | AND | OR | XOR | SLLW | SRLW | SRAW | SLT | SLTU) ? RD_RK :
+                        (BEQ | BNE | BLT | BLTU | BGE | BGEU) ? RD_RD :
+                        1'b0;
+
+    assign wD_sel =     (ADDW | SUBW | AND | OR | XOR | SLLW | SRLW | SRAW | SLT | SLTU
+                        | SLLIW | SRLIW | SRAIW
+                        | ADDIW | ANDI | ORI | XORI | SLTI | SLTUI
+                        | PCADDU) ? WD_ALU :
+                        (LDB | LDH) ? WD_SEXT2 :
+                        (LDBU) ? WD_DRAM_8 :
+                        (LDHU) ? WD_DRAM_16 : 
+                        (LDW) ? WD_DRAM_32 :
+                        (LU12IW) ? WD_INST :
+                        (JIRL) ? WD_PC4_RD :
+                        (BL) ? WD_PC4_R1 :
+                        3'b000;
+
 endmodule
